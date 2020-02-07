9e3c6b24d7168fb645f4abcbdd83a3b14c9a1f0d
Julian Moreno Schneider <jmschnei@gmail.com>
2020-02-07 11:59:52 +0100

    Added to have generic Format and Language enums

---
+++ src/main/java/de/dfki/cwm/data/Format.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package de.dfki.cwm.data;
+
+/**
+ * @author julianmorenoschneider
+ * @project CurationWorkflowManager
+ * @date 07.02.2020
+ * @company DFKI
+ * @description This enum determines specific formats for information in the workflow and workflow manager. 
+ * 
+ */
+public enum Format {
+
+	TURTLE,
+	RDF, 
+	JSONLD, 
+	TEXT, 
+	JSON, 
+	RDFXML, 
+	URI
+
+}
