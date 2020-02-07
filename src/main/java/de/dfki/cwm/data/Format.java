8638d790d73bb378112f76c4127b7d2074fc04f0
Julian Moreno Schneider <jmschnei@gmail.com>
2020-02-07 12:33:08 +0100

    Adding conversion from String to Format

--- src/main/java/de/dfki/cwm/data/Format.java
+++ src/main/java/de/dfki/cwm/data/Format.java
@@ -3,6 +3,11 @@
  */
 package de.dfki.cwm.data;
 
+import java.util.Arrays;
+import java.util.List;
+
+import de.dfki.cwm.exceptions.WorkflowException;
+
 /**
  * @author julianmorenoschneider
  * @project CurationWorkflowManager
@@ -19,6 +24,60 @@ public enum Format {
 	TEXT, 
 	JSON, 
 	RDFXML, 
-	URI
+	URI;
+	
+	public static final String[] turtle = {"turtle","TURTLE","TTL","ttl","text/turtle"};
+	public static final List<String> turtle2 = Arrays.asList(turtle);
+
+	public static final String[] rdf = {"rdf","RDF","application/rdf"};
+	public static final List<String> rdf2 = Arrays.asList(rdf);
+
+	public static final String[] jsonld = {"jsonld","JSONLD","json-ld","JSON-LD","application/ld+json"};
+	public static final List<String> jsonld2 = Arrays.asList(turtle);
+
+	public static final String[] text = {"text","TEXT","text/plain","plaintext","PLAINTEXT"};
+	public static final List<String> text2 = Arrays.asList(turtle);
+
+	public static final String[] json = {"json","JSON","application/json"};
+	public static final List<String> json2 = Arrays.asList(turtle);
+
+	public static final String[] rdfxml = {"rdfxml","RDFXML","application/xml+rdf"};
+	public static final List<String> rdfxml2 = Arrays.asList(turtle);
+
+	public static final String[] uri = {"uri","URI"};
+	public static final List<String> uri2 = Arrays.asList(turtle);
+
+	/**
+	 * @method-description  Translate a string into an enum element
+	 * @author  			julianmorenoschneider
+	 * @modified_by
+	 * @date  				07.02.2020
+	 * @return  			Enum type of the corresponding format
+	 */
+	public static Format getFormat(String sFormat) throws Exception {
+		if(turtle2.contains(sFormat)) {
+			return TURTLE;
+		}
+		if(rdf2.contains(sFormat)) {
+			return RDF;
+		}
+		if(jsonld2.contains(sFormat)) {
+			return JSONLD;
+		}
+		if(text2.contains(sFormat)) {
+			return TEXT;
+		}
+		if(json2.contains(sFormat)) {
+			return JSON;
+		}
+		if(rdfxml2.contains(sFormat)) {
+			return RDFXML;
+		}
+		if(uri2.contains(sFormat)) {
+			return URI;
+		}
+		throw new WorkflowException("Unsupported format: "+sFormat);
+	}
+
 
 }
