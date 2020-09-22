4ae74bfb67c73074751638200fdae5a6a71f4bd7
Julian Moreno Schneider <jmschnei@gmail.com>
2020-09-22 11:57:13 +0200

    Include file storage to manage multimedia files(audio, image, video)

---
+++ src/main/java/de/dfki/cwm/storage/FileStorage.java
@@ -0,0 +1,129 @@
+package de.dfki.cwm.storage;
+
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.StandardCopyOption;
+import java.util.Date;
+import java.util.HashMap;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import de.dfki.cwm.data.Format;
+import de.dfki.cwm.data.Format.*;
+
+@SuppressWarnings("unused")
+@Component
+public class FileStorage {
+
+	HashMap<String, File> temporaryFiles = new HashMap<String, File>();
+
+	public FileStorage() {
+	}
+
+	@PostConstruct
+	public void setup() {
+	}
+
+	public String generateTemporaryFile(InputStream is,Format iFormat) {
+		String fileId = "";
+		String fileSuffix = "";
+		fileId = (new Date().getTime())+"";
+		switch (iFormat) {
+		case WAV:
+			fileSuffix = "_tmp.wav";
+			break;
+		case MP3:
+			fileSuffix = "_tmp.mp3";
+			break;
+		case UNK:
+			fileSuffix = "_tmp.unk";
+			break;
+		default:
+			break;
+		}
+		try {
+			File tempFile = File.createTempFile("qwm-", fileSuffix);
+			//File tempFile = File.createTempFile("MyAppName-", ".tmp");
+			System.out.println(tempFile.getAbsolutePath());
+			java.nio.file.Files.copy(
+					is, 
+					tempFile.toPath(), 
+					StandardCopyOption.REPLACE_EXISTING);
+			//IOUtils.closeQuietly(is);
+			tempFile.deleteOnExit();
+			temporaryFiles.put(fileId, tempFile);
+			return fileId;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public String generateTemporaryFile(MultipartFile file,Format iFormat) {
+		String fileId = "";
+		String fileSuffix = "";
+		fileId = (new Date().getTime())+"";
+		try {
+			File tempFile = File.createTempFile("qwm-", file.getName());
+			//File tempFile = File.createTempFile("MyAppName-", ".tmp");
+			System.out.println(tempFile.getAbsolutePath());
+			java.nio.file.Files.copy(
+					file.getInputStream(), 
+					tempFile.toPath(), 
+					StandardCopyOption.REPLACE_EXISTING);
+			//IOUtils.closeQuietly(is);
+			tempFile.deleteOnExit();
+			temporaryFiles.put(fileId, tempFile);
+			return fileId;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public String generateTemporaryFile(String s,Format iFormat) {
+		String fileId = "";
+		String fileSuffix = "";
+		fileId = (new Date().getTime())+"";
+		switch (iFormat) {
+		case WAV:
+			fileSuffix = "_tmp.wav";
+			break;
+		case MP3:
+			fileSuffix = "_tmp.mpg";
+			break;
+		case UNK:
+			fileSuffix = "_tmp.unk";
+			break;
+		default:
+			break;
+		}
+		try {
+			File tempFile = File.createTempFile("qwm-", fileSuffix);
+			//File tempFile = File.createTempFile("MyAppName-", ".tmp");
+			FileUtils.writeStringToFile(tempFile, s, "UTF-8");
+			//IOUtils.closeQuietly(is);
+			tempFile.deleteOnExit();
+			System.out.println(tempFile.getAbsolutePath());
+			temporaryFiles.put(fileId, tempFile);
+			return fileId;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public File getFile(String id) {
+		if(temporaryFiles.containsKey(id)) {
+			return temporaryFiles.get(id);
+		}
+		return null;
+	}
+}
